b7ed4a590e1393194d6f9a7bc88735cc60972d33
jittipon <ti_jittipon@hotmail.com>
2022-12-02 04:09:51 +0700

    add chip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AppComponent } from './app.component';
 import { StyleClassModule } from 'primeng/styleclass';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+
 import { PageoneComponent } from './pageone/pageone.component';
 import { PagetwoComponent } from './pagetwo/pagetwo.component';
 import { PagethreeComponent } from './pagethree/pagethree.component';
+import { ChipModule } from 'primeng/chip';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,8 @@ import { PagethreeComponent } from './pagethree/pagethree.component';
     AppRoutingModule,
     StyleClassModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    ChipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pagetwo/pagetwo.component.ts
+++ src/app/pagetwo/pagetwo.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagetwo',
   templateUrl: './pagetwo.component.html',
   styleUrls: ['./pagetwo.component.scss']
 })
-export class PagetwoComponent {
+export class PagetwoComponent implements OnInit {
+
+
+  options: any;
+  // overlays: any[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.options = {
+      center: { lat: 36.890257, lng: 30.707417 },
+      zoom: 12
+    };
+  }
+
 
 }
